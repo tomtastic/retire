de8b403b9a8c80105220c0443104803e2163f392
Tom Matthews <tom.matthews@theguardian.com>
2026-08-12 07:01:08 +0100

    Add ESLint configuration for SARIF scan

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+};
